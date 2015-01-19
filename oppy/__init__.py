d00461e88dc1198745e0ca3bd8aad03dfc5becb0
Nik Kinkel <nskinkel@iastate.edu>
2015-01-19 16:14:20 -0600

    rebuild docs

--- oppy/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2014, 2015, Nik Kinkel
-# See LICENSE for licensing information
-
-import os
-base_dir = os.path.dirname(os.path.abspath(__file__))
--- oppy/path/exceptions.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2014, 2015, Nik Kinkel
-# See LICENSE for licensing information
-
-
-class UnknownPathConstraint(Exception):
-    pass
