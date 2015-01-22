b4c412e2f259e5ac7702bb1e36ee17dd4e6ba554
Nik Kinkel <nskinkel@iastate.edu>
2015-01-22 02:58:30 -0600

    add input parsing checks to Create2Cell

--- oppy/cell/fixedlen.py
+++ oppy/cell/fixedlen.py
@@ -216,17 +216,31 @@ class Create2Cell(FixedLenCell):
             raise BadPayloadData(msg)
 
         self.htype = struct.unpack('!H', data[offset:offset + HTYPE_LEN])[0]
+        
+        if self.htype != DEF.NTOR_HTYPE:
+            msg = "Create2 got htype: {}, but oppy only supports ntor: {}."
+            raise BadPayloadData(msg.format(self.htype, DEF.NTOR_HTYPE))
+
         offset += HTYPE_LEN
 
         self.hlen = struct.unpack('!H', data[offset:offset + HLEN_LEN])[0]
+
+        if self.hlen != DEF.NTOR_HLEN:
+            msg = "Create2 got hlen: {}, but oppy only supports ntor hlen: {}."
+            raise BadPayloadData(msg.format(self.hlen, DEF.NTOR_HLEN))
+
         offset += HLEN_LEN
 
         try:
             self.hdata = data[offset:offset + self.hlen]
         except IndexError:
-            msg = "Create2Cell hlen was specified to be {} bytes, but actual "
+            msg = "Create2 hlen was specified to be {} bytes, but actual "
             msg += "but hdata was {} bytes."
             raise BadPayloadData(msg.format(self.hlen, len(data) - offset))
+
+        if len(self.hdata) != self.hlen:
+            msg = "Create2 expcted len(hdata) to be 84 for ntor, got: {}."
+            raise BadPayloadData(msg.format(len(self.hdata)))
 
     def __repr__(self):
         fmt = '{}, htype={}, hlen={}, hdata={}'
