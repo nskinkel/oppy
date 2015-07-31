5d97c415e3df9a607d09445a9c7b48664fc8c4e2
Nik Kinkel <nskinkel@iastate.edu>
2015-07-30 22:35:06 -0700

    update circuitbuildtask to be simpler
    CircuitBuildTask is now a much smaller class, relying mainly on smaller
    standalone functions to do most of the work. A CircuitBuildTask now just
    handles setting up a callback chain of processing functions and notifying
    the proper managers on ultimate build success or failure.

--- oppy/circuit/circuitbuildtask.py
+++ oppy/circuit/circuitbuildtask.py
@@ -16,12 +16,18 @@ from oppy.cell.relay import RelayExtend2Cell, RelayExtended2Cell
 from oppy.cell.util import LinkSpecifier
 from oppy.circuit.circuit import Circuit
 from oppy.circuit.definitions import CircuitType
+from oppy.cell.definitions import (
+    CREATED2_CMD,
+    DESTROY_CMD,
+    RELAY_EXTENDED2_CMD,
+)
+
+
+class BuildTaskDestroyed(Exception):
+    pass
 
 
 # Major TODO's:
-#               - catch/handle crypto exceptions explicitly
-#               - catch/handle connection.send exceptions explicitly
-#               - catch/handle specific getPath exceptions
 #               - handle cells with unexpected origins
 #               - docs
 #               - figure out where alreadyCalledError is coming from when
@@ -29,49 +35,58 @@ from oppy.circuit.definitions import CircuitType
 class CircuitBuildTask(object):
 
     def __init__(self, connection_manager, circuit_manager, netstatus,
-                 guard_manager, _id, circuit_type=None, request=None,
-                 autobuild=True):
-        self._connection_manager = connection_manager
-        self._circuit_manager = circuit_manager
-        self._netstatus = netstatus
-        self._guard_manager = guard_manager
-        self.circuit_id = _id
+        guard_manager, _id, circuit_type=None, request=None, autobuild=True):
+
+        self.connection_manager = connection_manager
+        self.circuit_manager = circuit_manager
+        self.netstatus = netstatus
+        self.guard_manager = guard_manager
+        self.id = _id
         self.circuit_type = circuit_type
         self.request = request
-        self._hs_state = None
-        self._path = None
-        self._conn = None
-        self._crypt_path = []
+        self.handshake_state = None
+        self.path = None
+        self.conn = None
+        self.crypt_path = []
         self._read_queue = defer.DeferredQueue()
-        self._autobuild = autobuild
-        self._tasks = None
-        self._building = False
-        self._current_task = None
+        self.autobuild = autobuild
+        self.tasks = None
+        self.current_task = None
+        self._canceled = False
 
         if autobuild is True:
             self.build()
 
     def build(self):
-        if self._building is True:
-            msg = "Circuit {} already started build process."
-            raise RuntimeError(msg.format(self.circuit_id))
-
         try:
             # TODO: update for stable/fast flags based on circuit_type
-            self._tasks = path.getPath(self._netstatus, self._guard_manager,
-                                       exit_request=self.request)
+            self.tasks = path.getPath(self.netstatus, self.guard_manager,
+                exit_request=self.request)
+            if self._canceled:
+                # we may have been destroyed while waiting for a path
+                raise BuildTaskDestroyed()
         except Exception as e:
-            self._buildFailed(e)
+            _buildFailed(e, self)
             return
 
-        self._current_task = self._tasks
-        self._tasks.addCallback(self._build)
-        self._tasks.addCallback(self._buildSucceeded)
-        self._tasks.addErrback(self._buildFailed)
-        self._building = True
+        self.current_task = self.tasks
+        self.tasks.addCallback(self._build)
+        self.tasks.addCallback(_buildSucceeded, self)
+        self.tasks.addErrback(_buildFailed, self)
+
+    def _build(self, chosen_path):
+        self.path = chosen_path
+        d = _getConnection(self, self.path.entry)
+        self.current_task = d
+        d.addCallback(_sendCreate2Cell, self, self.path.entry)
+        d.addCallback(_deriveCreate2CellSecrets, self, self.path.entry)
+        for node in self.path[1:]:
+            d.addCallback(_sendExtend2Cell, self, node)
+            d.addCallback(_deriveExtend2CellSecrets, self, node)
+        return d
 
     def canHandleRequest(self, request):
-        if self._path is None:
+        if self.path is None:
             if request.is_host:
                 return True
             elif request.is_ipv4:
@@ -79,119 +94,94 @@ class CircuitBuildTask(object):
             else:
                 return self.circuit_type == CircuitType.IPv6
         else:
-            return self._path.exit.microdescriptor.exit_policy.can_exit_to(port=request.port)
+            return self.path.exit.microdescriptor.exit_policy.can_exit_to(
+                port=request.port)
 
     def recv(self, cell):
         self._read_queue.put(cell)
 
+    def recvCell(self):
+        self.current_task = self._read_queue.get()
+        return self.current_task
+
     def destroyCircuitFromManager(self):
-        msg = "CircuitBuildTask {} destroyed from manager."
-        msg = msg.format(self.circuit_id)
-        self._current_task.errback(Failure(Exception(msg)))
+        if self.current_task:
+            self.current_task.errback(Failure(BuildTaskDestroyed(
+                "CircuitBuildTask {} destroyed from manager.".format(self.id))))
+        self._canceled = True
 
     def destroyCircuitFromConnection(self):
-        msg = "CircuitBuildTask {} destroyed from connection."
-        msg = msg.format(self.circuit_id)
-        self._current_task.errback(Failure(Exception(msg)))
+        if self.current_task:
+            self.current_task.errback(Failure(BuildTaskDestroyed(
+                "CircuitBuildTask {} destroyed from connection.".format(self.id))))
+        self._canceled = True
 
-    def _recvCell(self, _):
-        self._current_task = self._read_queue.get()
-        return self._current_task
 
-    # NOTE: no errbacks are added because exceptions thrown in this inner
-    #       deferred will fire the errback added to the outer deferred
-    def _build(self, cpath):
-        self._path = cpath
-        d = self._getConnection(self._path.entry)
-        self._current_task = d
-        d.addCallback(self._sendCreate2Cell, self._path.entry)
-        d.addCallback(self._recvCell)
-        d.addCallback(self._deriveCreate2CellSecrets, self._path.entry)
-        for path_node in self._path[1:]:
-            d.addCallback(self._sendExtend2Cell, path_node)
-            d.addCallback(self._recvCell)
-            d.addCallback(self._deriveExtend2CellSecrets, path_node)
-        return d
+def _getConnection(task, node):
+    d = task.connection_manager.getConnection(node.router_status_entry)
+    task.current_task = d
+    def addCircuit(connection_result):
+        task.conn = connection_result
+        task.conn.addCircuit(task)
+    d.addCallback(addCircuit)
+    return d
 
-    def _getConnection(self, path_node):
 
-        d = self._connection_manager.getConnection(path_node.router_status_entry)
-        self._current_task = d
-        def addCirc(res):
-            self._conn = res
-            self._conn.addCircuit(self)
-            return res
-        d.addCallback(addCirc)
-        return d
+def _buildSucceeded(_, task):
+    circuit = Circuit(task.circuit_manager, task.id, task.conn,
+        task.circuit_type, task.path, task.crypt_path)
+    task.conn.addCircuit(circuit)
+    task.circuit_manager.circuitOpened(circuit)
 
-    def _sendCreate2Cell(self, _, path_node):
-        self._hs_state = ntor.NTorState(path_node.microdescriptor)
-        onion_skin = ntor.createOnionSkin(self._hs_state)
-        create2 = Create2Cell.make(self.circuit_id, hdata=onion_skin)
-        self._conn.send(create2)
 
-    def _deriveCreate2CellSecrets(self, response, path_node):
-        if isinstance(response, DestroyCell):
-            msg = ("DestroyCell received from {}."
-                   .format(path_node.router_status_entry.fingerprint))
-            raise ValueError(msg)
-        if not isinstance(response, Created2Cell):
-            msg = ("Unexpected cell {} received from {}."
-                   .format(response,
-                           path_node.router_status_entry.fingerprint))
-            destroy = DestroyCell.make(self.circuit_id)
-            self._conn.send(destroy)
-            raise ValueError(msg)
+def _buildFailed(reason, task):
+    logging.debug("Pending circuit {} failed. Reason: {}."
+                  .format(task.id, reason))
+    if task.conn is not None:
+        task.conn.removeCircuit(task.id)
+    task.circuit_manager.circuitDestroyed(task)
 
-        self._crypt_path.append(ntor.deriveRelayCrypto(self._hs_state,
-            response))
-        # TODO: implement this
-        #self._hs_state.memwipe()
-        self._hs_state = None
 
-    def _sendExtend2Cell(self, _, path_node):
-        lspecs = [LinkSpecifier(path_node),
-                  LinkSpecifier(path_node, legacy=True)]
-        self._hs_state = ntor.NTorState(path_node.microdescriptor)
-        onion_skin = ntor.createOnionSkin(self._hs_state)
-        extend2 = RelayExtend2Cell.make(self.circuit_id, nspec=len(lspecs),
-                                        lspecs=lspecs, hdata=onion_skin)
-        crypt_cell = crypto.encryptCell(extend2, self._crypt_path,
-                                        early=True)
-        self._conn.send(crypt_cell)
+def _sendCreate2Cell(_, task, node):
+    task.handshake_state = ntor.NTorState(node.microdescriptor)
+    onion_skin = ntor.createOnionSkin(task.handshake_state)
+    create2 = Create2Cell.make(task.id, hdata=onion_skin)
+    task.conn.send(create2)
+    return task.recvCell()
 
-    def _deriveExtend2CellSecrets(self, response, path_node):
-        if isinstance(response, DestroyCell):
-            msg = ("Destroy cell received from {} on pending circuit {}."
-                   .format(path_node.router_status_entry.fingerprint,
-                   self.circuit_id))
-            raise ValueError(msg)
 
-        cell, _ = crypto.decryptCell(response, self._crypt_path)
+def _sendExtend2Cell(_, task, node):
+    lspecs = [LinkSpecifier(node), LinkSpecifier(node, legacy=True)]
+    task.handshake_state = ntor.NTorState(node.microdescriptor)
+    onion_skin = ntor.createOnionSkin(task.handshake_state)
+    extend2 = RelayExtend2Cell.make(task.id, nspec=len(lspecs), lspecs=lspecs,
+        hdata=onion_skin)
+    crypt_cell = crypto.encryptCell(extend2, task.crypt_path, early=True)
+    task.conn.send(crypt_cell)
+    return task.recvCell()
 
-        if not isinstance(cell, RelayExtended2Cell):
-            msg = ("CircuitBuildTask {} received an unexpected cell: {}. "
-                   "Destroying the circuit."
-                   .format(self.circuit_id, type(cell)))
-            destroy = DestroyCell.make(self.circuit_id)
-            self._conn.send(destroy)
-            raise ValueError(msg)
 
-        self._crypt_path.append(ntor.deriveRelayCrypto(self._hs_state, cell))
-        # TODO: implement this
-        #self._hs_state.memwipe()
-        self._hs = None
+def _deriveCreate2CellSecrets(response, task, node):
+    _checkResponseCmd(response, node, task, cmd=CREATED2_CMD)
+    crypt_node = ntor.deriveRelayCrypto(task.handshake_state, response)
+    task.crypt_path.append(crypt_node)
 
-    def _buildSucceeded(self, _):
-        circuit = Circuit(self._circuit_manager, self.circuit_id, self._conn,
-                          self.circuit_type, self._path, self._crypt_path)
-        self._conn.addCircuit(circuit)
-        self._circuit_manager.circuitOpened(circuit)
 
-    def _buildFailed(self, reason):
-        msg = ("Pending circuit {} failed. Reason: {}."
-               .format(self.circuit_id, reason))
-        logging.debug(msg)
-        if self._conn is not None:
-            self._conn.removeCircuit(self.circuit_id)
-        self._circuit_manager.circuitDestroyed(self)
+def _deriveExtend2CellSecrets(response, task, node):
+    cell, _ = crypto.decryptCell(response, task.crypt_path)
+    _checkResponseCmd(cell, node, task, rcmd=RELAY_EXTENDED2_CMD)
+    crypt_node = ntor.deriveRelayCrypto(task.handshake_state, cell)
+    task.crypt_path.append(crypt_node)
+
+
+def _checkResponseCmd(response, node, task, cmd=None, rcmd=None):
+    fprint = node.router_status_entry.fingerprint
+    if response.header.cmd == DESTROY_CMD:
+        raise ValueError("DestroyCell received from {}".format(fprint))
+    fail = False
+    if (cmd and response.header.cmd != cmd) or \
+       (rcmd and response.rheader.cmd != rcmd):
+        destroy = DestroyCell.make(task.id)
+        task.conn.send(destroy)
+        raise ValueError("Unexpected {} received from {}.".format(
+            type(response), fprint))
